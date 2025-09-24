e449d1f6b0e72011a0ecf0dddcda901d3d39b44a
alfonsojan <alfonsobusker@gmail.com>
2025-09-24 13:14:49 +0200

    Added a hotkey to start the macro

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
 
 #include "parse.h"
 #include "action.h"
@@ -7,15 +8,19 @@
 #define shift_args(argc, argv) ((argc) > 0 ? ((argc)--, *(argv)++) : NULL)
 
 void usage(const char *program) {
-    fprintf(stderr, "Usage: %s <macro script> [OPTIONS...] \n", program);
+    fprintf(stderr, "Usage: %s <macro script> [OPTIONS...]\n", program);
     fprintf(stderr, "Options:\n");
-    fprintf(stderr, "   -h                  print this help and exit\n");
-    fprintf(stderr, "   -c | --commands     print out all the command\n");
+    fprintf(stderr, "   -h                      print this help and exit\n");
+    fprintf(stderr, "   -c | --commands         print out all the commands\n");
+    fprintf(stderr, "   -i | --immediate        start immediately (do not wait for Alt+F12)\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "Default behavior is to arm and wait for Alt+F12, then run the macro.\n");
 }
 
 int main(int argc, char **argv) {
     const char *program = shift_args(argc, argv);
     const char *macro_path = shift_args(argc, argv);
+    int wait_for_hotkey = 1;
 
     if (macro_path == NULL) {
         usage(program);
@@ -30,6 +35,8 @@ int main(int argc, char **argv) {
         } else if (strcmp(flag, "-c") == 0 || strcmp(flag, "--commands") == 0) {
             print_out_action_type_map();
             return 0;
+        } else if (strcmp(flag, "-i") == 0 || strcmp(flag, "--immediate") == 0) {
+            wait_for_hotkey = 0;
         } else {
             fprintf(stderr, "Unknown command: '%s'\n", flag);
             usage(program);
@@ -41,6 +48,29 @@ int main(int argc, char **argv) {
     size_t action_count = 0;
     if (parse_macro_text(macro_path, actions, &action_count) != 0) {
         return 1;
+    }
+
+    if (wait_for_hotkey) {
+        // ALT + F12
+        if (!RegisterHotKey(NULL, 1, MOD_ALT, VK_F12)) {
+            DWORD err = GetLastError();
+            if (err == ERROR_HOTKEY_ALREADY_REGISTERED /*1409*/) {
+                fprintf(stderr, "Failed to register hotkey: Alt+F12 is already in use.\n");
+            } else {
+                fprintf(stderr, "Failed to register hotkey (error %lu).\n", err);
+            }
+            free_actions(actions, action_count);
+            return 1;
+        }
+
+        printf("AutoStroke armed. Press Alt+F12 to start...\n");
+        MSG msg;
+        while (GetMessage(&msg, NULL, 0, 0) != 0) {
+            if (msg.message == WM_HOTKEY && msg.wParam == 1) {
+                break;
+            }
+        }
+        UnregisterHotKey(NULL, 1);
     }
 
     if (run_actions(actions, action_count) != 0) {
